61e9b94f8cca2b1712ba553d15ce3f4ad5d85c25
lubuntu <lubuntu@lubuntu-VirtualBox.(none)>
2013-02-07 15:06:07 +0200

    normaalne uuendus tuli jälle

---
+++ pages/about.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lubuntu
+ * Date: 2/7/13
+ * Time: 1:28 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "KHK ametlik kodulehekylg";
+
---
+++ pages/signup.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lubuntu
+ * Date: 2/7/13
+ * Time: 1:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+?>
+
+<form method="post" action="?page=regatud.php">
+    First name: <input type="text" name="firstname"><br>
+    Last name: <input type="text" name="lastname"><br>
+    Email: <input type="text" name="email"><br>
+    <input type="submit" value="Submit">
+</form>
+
